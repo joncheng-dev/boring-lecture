a7e8ca0d8fc775188fa9e9037b65c64aab670476
Jonathan Cheng <jcheng461@gmail.com>
2022-02-21 20:52:33 +0900

    add text color changes for dark and light themes that change with push of buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-  $("button#dark").click(function(){
+$(document).ready(function () {
+  $("button#dark").click(function () {
     $("body").removeClass();
     $("body").addClass("dark-background");
+    $("p").css({ color: "#F0F0F0" });
+    $(".intro").css({ color: "yellow" });
   });
-  $("button#light").click(function(){
+  $("button#light").click(function () {
     $("body").removeClass();
     $("body").addClass("light-background");
+    $("p").css({ color: "black" });
+    $(".intro").css({ color: "blueviolet" });
   });
-})
+});
